6a45a65701ba275d56a60cac67e4d51dab5503f4
Judzhin Miles <info@msbios.com>
2020-12-08 12:24:47 +0200

    Intermedaite commit

---
+++ oop/oop.js
@@ -0,0 +1,152 @@
+/**
+ *
+ * @type {{DEFAULTS, ready, _init}}
+ */
+var APP = (function ($) {
+
+    /**
+     *
+     * @type {{}}
+     */
+    var defaultOptions = {};
+
+    /**
+     *
+     * @param options
+     * @constructor
+     */
+    var A = function (options) {
+        this.$options = $.extend({}, this.DEFAULTS, options);
+        console.log('Class A, Constructor');
+    };
+
+    A.prototype = {
+        DEFAULTS: {
+            propertyA: 'A',
+            propertyB: 'B'
+        },
+
+        methodA: function () {
+            console.log('Class A, Method A');
+        },
+
+        methodB: function () {
+            console.log('Class A, Method B');
+        },
+
+        methodOptions: function () {
+            console.log(this.$options);
+        }
+    };
+
+    /**
+     *
+     * @param options
+     * @constructor
+     */
+    var B = function (options) {
+        this.$options = $.extend({}, this.DEFAULTS, options);
+        console.log('Class B, Constructor');
+    };
+
+    B.prototype = $.extend(true, {
+        DEFAULTS: {
+            propertyC: 'C'
+        },
+        methodC: function () {
+            console.log('Class B, Method C');
+        }
+    }, A.prototype);
+
+    /**
+     *
+     * @param options
+     * @constructor
+     */
+    var C = function (options) {
+        this.$options = $.extend({}, this.DEFAULTS, options);
+        console.log('Class C, Constructor');
+    };
+
+    C.prototype = {
+        DEFAULTS: {
+            propertyC: 'C'
+        },
+        methodC: function () {
+            console.log('Class C, Method C');
+        },
+        methodOptions: function () {
+            console.log(this.$options);
+        }
+    };
+
+    $.extend(true, C.prototype, B.prototype);
+
+    C.prototype.methodC.apply(C, arguments);
+
+    return {
+        DEFAULTS: {
+            // Default Properties
+        },
+
+        /**
+         *
+         * @param options
+         */
+        ready: function (options) {
+
+            var self = this, $initializer = $.Deferred(function (deffered) {
+                $(function () {
+                    deffered.resolve.call(self, deffered.options);
+                });
+            });
+
+            this.$options = $.extend({}, this.DEFAULTS, options);
+
+            $.each(this.initialize, function (name) {
+                if (name in self.$options) {
+                    self.initialize[name](self.$options[name]);
+                    // $initializer.options = self.$options[name];
+                    // $initializer.then(this, $initializer.options);
+                }
+            });
+        },
+
+        initialize: {
+            /**
+             *
+             * @param options
+             */
+            classA: function (options) {
+                var a = new A(options);
+                a.methodA();
+                a.methodB();
+                a.methodOptions();
+            },
+
+            /**
+             *
+             * @param options
+             */
+            classB: function (options) {
+                var b = new B(options);
+                b.methodA();
+                b.methodB();
+                b.methodC();
+                b.methodOptions();
+            },
+
+            /**
+             *
+             * @param options
+             */
+            classC: function (options) {
+                var c = new C(options);
+                c.methodA();
+                c.methodB();
+                c.methodC();
+                c.methodOptions();
+            }
+        }
+    }
+}).call(this, jQuery);
